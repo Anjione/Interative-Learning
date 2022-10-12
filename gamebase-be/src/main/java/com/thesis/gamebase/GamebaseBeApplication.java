d287bfa9d321521f2cee25fd41642683f54e993a
Nguyễn Đăng An <annd@vissoft.vn>
2022-10-12 16:46:41 +0700

    init project

---
+++ gamebase-be/src/main/java/com/thesis/gamebase/GamebaseBeApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.gamebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebaseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamebaseBeApplication.class, args);
+    }
+
+}
---
+++ gamebase-be/src/test/java/com/thesis/gamebase/GamebaseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesis.gamebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamebaseBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
