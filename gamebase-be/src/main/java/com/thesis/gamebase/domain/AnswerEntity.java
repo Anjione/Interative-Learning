1ae67f9c830f71b06d0520daef74a9bc913b6ab8
Nguyễn Đăng An <annd@vissoft.vn>
2022-10-28 00:35:52 +0700

    update api getAllQuestion

---
+++ gamebase-be/src/main/java/com/thesis/gamebase/domain/AnswerEntity.java
@@ -0,0 +1,30 @@
+package com.thesis.gamebase.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+
+@Data
+@Valid
+@Table(name = "answer")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "question_id")
+    private int questionId;
+
+    @Column(name = "answer_name")
+    private String answerName;
+
+    @Column(name = "correct_answer")
+    private int correctAnswer;
+}
