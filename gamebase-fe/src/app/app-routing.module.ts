1c7fe0801971d9eb6612286e3c9148b71eb2e2f0
Nguyễn Đăng An <annd@vissoft.vn>
2022-10-31 07:13:43 +0700

    Cập nhật side nav cho giao diện

--- gamebase-fe/src/app/app-routing.module.ts
+++ gamebase-fe/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { DashboardComponent } from './sysadmin/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './sysadmin/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gamebase-fe/src/app/app.module.ts
+++ gamebase-fe/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SysadminComponent } from './sysadmin/sysadmin.component';
+import { SidenavComponent } from './sysadmin/sidenav/sidenav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './sysadmin/header/header.component';
+import { HomeComponent } from './sysadmin/home/home.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SysadminComponent,
+    SidenavComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // * MATERIAL IMPORTS
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
